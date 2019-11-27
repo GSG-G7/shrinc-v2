ba414c2198d31a4dbc26f36ad716b175703e1366
hashemt <hashemta77@gmail.com>
2019-11-27 08:58:21 +0200

    console success connection

--- server/database/config/mongodbConnection.js
+++ server/database/config/mongodbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const mongodbConnection = () => mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-k05rz.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true });
+const mongodbConnection = () => mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-k05rz.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }).then(() => console.log('connectio DB'));
 
 module.exports = mongodbConnection;
